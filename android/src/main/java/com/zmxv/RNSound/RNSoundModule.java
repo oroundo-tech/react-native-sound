8da1e8ddf1cbc60b14a0977bb9dbe2af190907ee
Zoran Marjanovic <zmarjanovic89@gmail.com>
2017-07-07 13:51:24 +0200

    Fixed issue with playing local files

--- android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
+++ android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
@@ -17,186 +17,193 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.io.IOException;
+
 import android.util.Log;
 
 public class RNSoundModule extends ReactContextBaseJavaModule {
-  Map<Integer, MediaPlayer> playerPool = new HashMap<>();
-  ReactApplicationContext context;
-  final static Object NULL = null;
+    Map<Integer, MediaPlayer> playerPool = new HashMap<>();
+    ReactApplicationContext context;
+    final static Object NULL = null;
 
-  public RNSoundModule(ReactApplicationContext context) {
-    super(context);
-    this.context = context;
-  }
-
-  @Override
-  public String getName() {
-    return "RNSound";
-  }
-
-  @ReactMethod
-  public void prepare(final String fileName, final Integer key, final Callback callback) {
-    MediaPlayer player = createMediaPlayer(fileName);
-    if (player == null) {
-      WritableMap e = Arguments.createMap();
-      e.putInt("code", -1);
-      e.putString("message", "resource not found");
-      callback.invoke(e);
-      return;
-    }
-    this.playerPool.put(key, player);
-    player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-      @Override
-      public void onPrepared(MediaPlayer mediaPlayer) {
-        WritableMap props = Arguments.createMap();
-        props.putDouble("duration", mediaPlayer.getDuration() * .001);
-        callback.invoke(NULL, props);
-      }
-    });
-    player.prepareAsync();
-  }
-
-  protected MediaPlayer createMediaPlayer(final String fileName) {
-    int res = this.context.getResources().getIdentifier(fileName, "raw", this.context.getPackageName());
-    if (res != 0) {
-      return MediaPlayer.create(this.context, res);
-    }
-    if(fileName.startsWith("http://") || fileName.startsWith("https://")) {
-      MediaPlayer mediaPlayer = new MediaPlayer();
-      mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-      Log.i("RNSoundModule", fileName);
-      try {
-        mediaPlayer.setDataSource(fileName);
-      } catch(IOException e) {
-        Log.e("RNSoundModule", "Exception", e);
-        return null;
-      }
-      return mediaPlayer;
+    public RNSoundModule(ReactApplicationContext context) {
+        super(context);
+        this.context = context;
     }
 
-    File file = new File(fileName);
-    if (file.exists()) {
-      Uri uri = Uri.fromFile(file);
-      return MediaPlayer.create(this.context, uri);
+    @Override
+    public String getName() {
+        return "RNSound";
     }
-    return null;
-  }
 
-  @ReactMethod
-  public void play(final Integer key, final Callback callback) {
-    MediaPlayer player = this.playerPool.get(key);
-    if (player == null) {
-      callback.invoke(false);
-      return;
-    }
-    if (player.isPlaying()) {
-      return;
-    }
-    player.setOnCompletionListener(new OnCompletionListener() {
-      boolean callbackWasCalled = false;
-
-      @Override
-      public synchronized void onCompletion(MediaPlayer mp) {
-        if (!mp.isLooping()) {
-          if (callbackWasCalled) return;
-          callbackWasCalled = true;
-          callback.invoke(true);
+    @ReactMethod
+    public void prepare(final String fileName, final Integer key, final Callback callback) {
+        MediaPlayer player = createMediaPlayer(fileName);
+        if (player == null) {
+            WritableMap e = Arguments.createMap();
+            e.putInt("code", -1);
+            e.putString("message", "resource not found");
+            callback.invoke(e);
+            return;
         }
-      }
-    });
-    player.setOnErrorListener(new OnErrorListener() {
-      boolean callbackWasCalled = false;
-
-      @Override
-      public synchronized boolean onError(MediaPlayer mp, int what, int extra) {
-        if (callbackWasCalled) return true;
-        callbackWasCalled = true;
-        callback.invoke(false);
-        return true;
-      }
-    });
-    player.start();
-  }
-
-  @ReactMethod
-  public void pause(final Integer key, final Callback callback) {
-    MediaPlayer player = this.playerPool.get(key);
-    if (player != null && player.isPlaying()) {
-      player.pause();
+        this.playerPool.put(key, player);
+        if (fileName.startsWith("http://") || fileName.startsWith("https://")) {
+            player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    WritableMap props = Arguments.createMap();
+                    props.putDouble("duration", mediaPlayer.getDuration() * .001);
+                    callback.invoke(NULL, props);
+                }
+            });
+            player.prepareAsync();
+        } else {
+            WritableMap props = Arguments.createMap();
+            props.putDouble("duration", player.getDuration() * .001);
+            callback.invoke(NULL, props);
+        }
     }
-    callback.invoke();
-  }
 
-  @ReactMethod
-  public void stop(final Integer key, final Callback callback) {
-    MediaPlayer player = this.playerPool.get(key);
-    if (player != null && player.isPlaying()) {
-      player.pause();
-      player.seekTo(0);
+    protected MediaPlayer createMediaPlayer(final String fileName) {
+        int res = this.context.getResources().getIdentifier(fileName, "raw", this.context.getPackageName());
+        if (res != 0) {
+            return MediaPlayer.create(this.context, res);
+        }
+        if (fileName.startsWith("http://") || fileName.startsWith("https://")) {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            Log.i("RNSoundModule", fileName);
+            try {
+                mediaPlayer.setDataSource(fileName);
+            } catch (IOException e) {
+                Log.e("RNSoundModule", "Exception", e);
+                return null;
+            }
+            return mediaPlayer;
+        }
+
+        File file = new File(fileName);
+        if (file.exists()) {
+            Uri uri = Uri.fromFile(file);
+            return MediaPlayer.create(this.context, uri);
+        }
+        return null;
     }
-    callback.invoke();
-  }
 
-  @ReactMethod
-  public void release(final Integer key) {
-    MediaPlayer player = this.playerPool.get(key);
-    if (player != null) {
-      player.release();
-      this.playerPool.remove(key);
+    @ReactMethod
+    public void play(final Integer key, final Callback callback) {
+        MediaPlayer player = this.playerPool.get(key);
+        if (player == null) {
+            callback.invoke(false);
+            return;
+        }
+        if (player.isPlaying()) {
+            return;
+        }
+        player.setOnCompletionListener(new OnCompletionListener() {
+            boolean callbackWasCalled = false;
+
+            @Override
+            public synchronized void onCompletion(MediaPlayer mp) {
+                if (!mp.isLooping()) {
+                    if (callbackWasCalled) return;
+                    callbackWasCalled = true;
+                    callback.invoke(true);
+                }
+            }
+        });
+        player.setOnErrorListener(new OnErrorListener() {
+            boolean callbackWasCalled = false;
+
+            @Override
+            public synchronized boolean onError(MediaPlayer mp, int what, int extra) {
+                if (callbackWasCalled) return true;
+                callbackWasCalled = true;
+                callback.invoke(false);
+                return true;
+            }
+        });
+        player.start();
     }
-  }
 
-  @ReactMethod
-  public void setVolume(final Integer key, final Float left, final Float right) {
-    MediaPlayer player = this.playerPool.get(key);
-    if (player != null) {
-      player.setVolume(left, right);
+    @ReactMethod
+    public void pause(final Integer key, final Callback callback) {
+        MediaPlayer player = this.playerPool.get(key);
+        if (player != null && player.isPlaying()) {
+            player.pause();
+        }
+        callback.invoke();
     }
-  }
 
-  @ReactMethod
-  public void setLooping(final Integer key, final Boolean looping) {
-    MediaPlayer player = this.playerPool.get(key);
-    if (player != null) {
-      player.setLooping(looping);
+    @ReactMethod
+    public void stop(final Integer key, final Callback callback) {
+        MediaPlayer player = this.playerPool.get(key);
+        if (player != null && player.isPlaying()) {
+            player.pause();
+            player.seekTo(0);
+        }
+        callback.invoke();
     }
-  }
 
-  @ReactMethod
-  public void setSpeed(final Integer key, final Float speed) {
-    MediaPlayer player = this.playerPool.get(key);
-    if (player != null) {
-      player.setPlaybackParams(player.getPlaybackParams().setSpeed(speed));
+    @ReactMethod
+    public void release(final Integer key) {
+        MediaPlayer player = this.playerPool.get(key);
+        if (player != null) {
+            player.release();
+            this.playerPool.remove(key);
+        }
     }
-  }
 
-  @ReactMethod
-  public void setCurrentTime(final Integer key, final Float sec) {
-    MediaPlayer player = this.playerPool.get(key);
-    if (player != null) {
-      player.seekTo((int)Math.round(sec * 1000));
+    @ReactMethod
+    public void setVolume(final Integer key, final Float left, final Float right) {
+        MediaPlayer player = this.playerPool.get(key);
+        if (player != null) {
+            player.setVolume(left, right);
+        }
     }
-  }
 
-  @ReactMethod
-  public void getCurrentTime(final Integer key, final Callback callback) {
-    MediaPlayer player = this.playerPool.get(key);
-    if (player == null) {
-      callback.invoke(-1, false);
-      return;
+    @ReactMethod
+    public void setLooping(final Integer key, final Boolean looping) {
+        MediaPlayer player = this.playerPool.get(key);
+        if (player != null) {
+            player.setLooping(looping);
+        }
     }
-    callback.invoke(player.getCurrentPosition() * .001, player.isPlaying());
-  }
 
-  @ReactMethod
-  public void enable(final Boolean enabled) {
-    // no op
-  }
+    @ReactMethod
+    public void setSpeed(final Integer key, final Float speed) {
+        MediaPlayer player = this.playerPool.get(key);
+        if (player != null) {
+            player.setPlaybackParams(player.getPlaybackParams().setSpeed(speed));
+        }
+    }
 
-  @Override
-  public Map<String, Object> getConstants() {
-    final Map<String, Object> constants = new HashMap<>();
-    constants.put("IsAndroid", true);
-    return constants;
-  }
+    @ReactMethod
+    public void setCurrentTime(final Integer key, final Float sec) {
+        MediaPlayer player = this.playerPool.get(key);
+        if (player != null) {
+            player.seekTo((int) Math.round(sec * 1000));
+        }
+    }
+
+    @ReactMethod
+    public void getCurrentTime(final Integer key, final Callback callback) {
+        MediaPlayer player = this.playerPool.get(key);
+        if (player == null) {
+            callback.invoke(-1, false);
+            return;
+        }
+        callback.invoke(player.getCurrentPosition() * .001, player.isPlaying());
+    }
+
+    @ReactMethod
+    public void enable(final Boolean enabled) {
+        // no op
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("IsAndroid", true);
+        return constants;
+    }
 }
